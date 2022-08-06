e54588e5888cbb3b49aa7a7824e477fe180d1f84
Jorge Luis Sampaio de Oliveira <jorgeluis.geek@gmail.com>
2022-08-05 22:23:30 -0300

    Application patterns designwith Maven,

---
+++ src/main/java/one/digitalinnovation/labpadroesprojetojavaspring/serv/ProductsService.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.labpadroesprojetojavaspring.services;
+
+import one.digitalinnovation.labpadroesprojetojavaspring.entities.Products;
+
+
+public interface ProductsService {
+
+    Iterable<Products> findAll();
+
+    Products findById(Long id);
+
+    void insert(Products product);
+
+    void update(Long id, Products product);
+
+    void delete(Long id);
+}
